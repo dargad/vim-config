ff1daf2050f0bd091729b39cc23350628beb68b1
Dariusz Gadomski <dgadomski@gmail.com>
2012-11-24 18:04:31 +0100

    Added setup script.

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PREV_PATH=$(pwd)
+
+VIMDIR=${HOME}/.vim
+
+cd ${VIMDIR}
+
+ln -sf ${VIMDIR}/vimrc ${HOME}/.vimrc
+mkdir -p ${HOME}/.fonts/
+ln -sf ${VIMDIR}/fonts/ubuntu-mono-powerline-ttf ${HOME}/.fonts/
+fc-cache -fs
+
+cd ${PREV_PATH}
